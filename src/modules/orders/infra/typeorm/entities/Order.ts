aa4a9612b038934e13c46b8e00f4532d0b5faa8c
Eduardo SO <eduardo.sool07@gmail.com>
2020-07-12 20:25:19 -0300

    add: Order entity

--- src/modules/orders/infra/typeorm/entities/Order.ts
+++ src/modules/orders/infra/typeorm/entities/Order.ts
@@ -11,15 +11,22 @@ import {
 import Customer from '@modules/customers/infra/typeorm/entities/Customer';
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('orders')
 class Order {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @OneToOne(() => Customer)
   customer: Customer;
 
+  @OneToMany(() => OrdersProducts, order => order.product)
+  @JoinColumn({ name: 'order_id' })
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
